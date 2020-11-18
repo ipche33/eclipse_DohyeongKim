bec75d129fb03d78dec50c3298d9bdff3f4bb21b
ipche33 <73188114+ipche33@users.noreply.github.com>
2020-11-18 22:24:38 +0900

    Update PushPrint.java

--- HelloWorld/src/HelloWorld/PushPrint.java
+++ HelloWorld/src/HelloWorld/PushPrint.java
@@ -5,9 +5,7 @@ public class PushPrint
 	public static void main(String[] args) 
 	{
 		System.out.println("sophomore");
-		System.out.println("Wankwang Univ.");
 		System.out.println("Jeonbuk Iksan");
-		System.out.println("fighting!");
 	}
 
 }
