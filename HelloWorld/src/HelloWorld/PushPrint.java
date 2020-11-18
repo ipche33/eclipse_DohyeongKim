c6889f3c43d0c51cd7ee18653e41a5a52f0d63c5
Dohyeong Kim <rlaehgud777@gmail.com>
2020-11-18 20:48:36 +0900

    print Grade

--- HelloWorld/src/HelloWorld/PushPrint.java
+++ HelloWorld/src/HelloWorld/PushPrint.java
@@ -4,7 +4,7 @@ public class PushPrint
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Sophomore");
-		System.out.println("Wankwang Univ.");
+		System.out.println("sophomore");
 	}
+
 }
