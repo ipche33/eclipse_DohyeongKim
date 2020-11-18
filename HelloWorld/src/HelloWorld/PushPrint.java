c823092058f32ac1fa570b1cfb046d807bf821d4
ipche33 <73188114+ipche33@users.noreply.github.com>
2020-11-18 22:29:25 +0900

    conflict

--- HelloWorld/src/HelloWorld/PushPrint.java
+++ HelloWorld/src/HelloWorld/PushPrint.java
@@ -5,11 +5,8 @@ public class PushPrint
 	public static void main(String[] args) 
 	{
 		System.out.println("sophomore");
-<<<<<<< HEAD
 		System.out.println("Wonkwang Univ.");
-=======
 		System.out.println("Jeonbuk Iksan");
->>>>>>> refs/remotes/origin/master
 	}
 
 }
